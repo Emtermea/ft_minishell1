e04c55ba5438ae5a01bfaf2a3a5198e7522278ab
Emmanuelle TERMEAU <emtermea@e2r9p9.42.fr>
2016-09-05 19:46:26 +0200

    Add echo builtin

--- srcs/builtin_echo.c
+++ srcs/builtin_echo.c
@@ -12,10 +12,55 @@
 
 #include "ft_sh1.h"
 
+/*char	*ft_strclean(char const *str)
+{
+	char	clear[4096];
+	int		i;
+	int		j;
+
+	i = -1;
+	j = 0;
+	if (str)
+	{
+		ft_memset(clear, '\0', 4096);
+		while (ft_isspace(*str))
+			str++;
+		if (!str || !*str)
+			return (NULL);
+		while (str[++i] && j < 4096)
+		{
+			if (!ft_isspace(str[i]))
+				clear[j++] = str[i];
+			else
+			{
+				if (str[i + 1] && !ft_isspace(str[i + 1]))
+					clear[j++] = ' ';
+			}
+		}
+	}
+	return (ft_strdup(clear));
+}*/
+
 int	builtin_echo(char **command, t_env **env)
 {
 	printf("%s\n", __func__);
-	(void)command;
 	(void)env;
+	int	i;
+	size_t	size;
+
+	i = 1;
+	if (command)
+	{
+		size = ft_strlen_tab(command);
+		while (command[i])
+		{
+			ft_putstr(command[i]);
+			i++;
+			if (i < (int)size)
+				ft_putchar(' ');
+		}
+		ft_putchar('\n');
+		return (0);
+	}
 	return (1);
 }
